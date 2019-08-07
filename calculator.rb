35d55e8c1ee00991c6bbc8761e8032505952626a
Lizzy Tong <lizzy.tong33@gmail.com>
2019-08-07 22:57:23 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,5 @@
 # Add your variables here
-first_number = Integer
-puts first_number
+first_number = 89
 #second_number = 9
 #sum = first_number + second_number
 #difference = first_number - second_number
